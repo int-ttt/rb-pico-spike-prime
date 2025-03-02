f0aa9ea4e565837e1e46871b5a41cb749da583f4
2500s <2500slee@gmail.com>
2025-03-02 23:26:37 +0900

    micropython serial talk (uart) test

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
-from machine import Pin
-import utime
+from machine import Pin, UART
+import ustruct
 
 led = Pin(25, Pin.OUT)
+v33 = Pin(3, Pin.OUT)
+ll = Pin(4)
+v33.on()
+led.on()
+u1 = UART(0, 115200)
+# u2 = UART(1, 115200)
+
+while ll.value() == 0:
+    pass
+u1.write(b"\xaa")
+# u2.write(b"\xaa")
 
 while True:
-    led.value(1)
-    utime.sleep(1)
-    led.value(0)
-    utime.sleep(1)
+    if u1.any() > 0:
+        print('u1', u1.any(), ustruct.unpack("<B", u1.read(1)))
+    # if u2.any() > 0:
+    #     print('u2', u2.any(), ustruct.unpack("<B", u2.read(1)))
